77dc2fa7aa59ec63ecf45ef4f8491f890941682c
gabsereniski <gabsereniski@gmail.com>
2022-10-10 14:45:55 -0300

    code refactoring

--- implementacao/automata.cpp
+++ implementacao/automata.cpp
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <iostream>
 #include <sstream>
 #include <fstream>
@@ -7,10 +8,12 @@ using namespace std;
 
 #define token second
 
+typedef string::iterator in;
+enum state {Q0, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, TRAP};
+bool valid(string list, char sym){ return list.find(sym) != string::npos; }
+
 ifstream input;
 ofstream output;
-
-enum state {Q0, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, TRAP};
 
 string blank_sym = "\n\t ";
 string op_sym =    "=+-*/<>!";
@@ -55,144 +58,126 @@ map<string, string> reserved_words = {
     {  "void", "VOID"  }
 };
 
-bool valid(string list, char sym){ return list.find(sym) != string::npos; }
-
-state q0(string in, int &i, string &buffer)
+state q0(in &sym, string &buffer)
 {
-    char sym = in[i];
-
-    if(valid(blank_sym, sym))
+    if(valid(blank_sym, *sym))
     {
-        i++;
+        sym++;
         return Q0; // inicial
     }
     
-    if(valid(alph_sym, sym))
+    if(valid(alph_sym, *sym))
     {
-        buffer += sym;
-        i++;
+        buffer += *(sym++);
         return Q1; // primeira letra
     }
 
-    if(valid(num_sym, sym))
+    if(valid(num_sym, *sym))
     {
-        buffer += sym;
-        i++;
+        buffer += *(sym++);
         return Q4; // primeiro numero
     }
 
-    if(valid(op_sym, sym))
+    if(valid(op_sym, *sym))
     {
-        buffer += sym;
-        i++;
+        buffer += *(sym++);
         return Q6; //primeiro operador
     }
 
-    if(valid(delim_sym, sym))
+    if(valid(delim_sym, *sym))
     {
-        buffer += sym;
-        i++;
+        buffer += *(sym++);
         return Q8; // delimitador
     }
 
     return TRAP;
 }
 
-state q1(string in, int &i, string &buffer) //li uma letra
+state q1(in &sym, string &buffer) //li uma letra
 {
-    char sym = in[i];
-
-    if(valid(blank_sym, sym)||valid(op_sym, sym)||valid(delim_sym, sym))
+    if(valid(blank_sym, *sym)||valid(op_sym, *sym)||valid(delim_sym, *sym))
     {
         if(reserved_words.find(buffer) != reserved_words.end())
             return Q2; //aceita reservado
         return Q3; // aceita id
     }
     
-    if(valid(alph_sym, sym)||valid(num_sym, sym))
+    if(valid(alph_sym, *sym)||valid(num_sym, *sym))
     {
-        buffer += sym;
-        i++;
+        buffer += *(sym++);
         return Q1; // outras letras
     }
 
     return TRAP;
 }
 
-state q2(string in, int &i, string &buffer) // reconhece reservado
+state q2(in &sym, string &buffer) // reconhece reservado
 {
     output << reserved_words[buffer] << endl;
     buffer.clear();
     return Q0;
 }
 
-state q3(string in, int &i, string &buffer) // reconhece id
+state q3(in &sym, string &buffer) // reconhece id
 {
     output << "ID" << endl;
     buffer.clear();
     return Q0;
 }
 
-state q4(string in, int &i, string &buffer) //li um numero
+state q4(in &sym, string &buffer) //li um numero
 {
-    char sym = in[i];
-
-    if(valid(blank_sym, sym)||valid(op_sym, sym)||valid(delim_sym, sym))
+    if(valid(blank_sym, *sym)||valid(op_sym, *sym)||valid(delim_sym, *sym))
         return Q5; // aceita numero
     
-    if(valid(alph_sym, sym))
+    if(valid(alph_sym, *sym))
     {
-        buffer += sym;
-        i++;
+        buffer += *(sym++);
         return TRAP; // erro ao ler letra depois de numero
     }
 
-    if(valid(num_sym, sym))
+    if(valid(num_sym, *sym))
     {
-        buffer += sym;
-        i++;
+        buffer += *(sym++);
         return Q4; // + numero
     }
 
     return TRAP;
 }
 
-state q5(string in, int &i, string &buffer) // reconhece numero
+state q5(in &sym, string &buffer) // reconhece numero
 {
     output << "NUMBER" << endl;
     buffer.clear();
     return Q0;
 }
 
-state q6(string in, int &i, string &buffer) //li um operador
+state q6(in &sym, string &buffer) //li um operador
 {
-    char sym = in[i];
-
-    if(valid(blank_sym, sym)||valid(alph_sym, sym)||valid(num_sym, sym))
+    if(valid(blank_sym, *sym)||valid(alph_sym, *sym)||valid(num_sym, *sym))
         return Q7; // aceita operador
 
-    if(valid(op_sym, sym))
+    if(valid(op_sym, *sym))
     {
-        buffer += sym;
-        i++;
+        buffer += *(sym++);
         if(buffer.length() > 2) return TRAP; //nao tem operadores com mais de 2 caracteres
         return Q6; //operador formado por + de um caracteres
     }
 
-    if(valid(delim_sym, sym))
+    if(valid(delim_sym, *sym))
         return TRAP; // nao da pra ler delimitador depois de operador
 
     return TRAP;
 }
 
-state q7(string in, int &i, string &buffer) // reconhece operador
+state q7(in &sym, string &buffer) // reconhece operador
 {
     output << operators[buffer] << endl;
     buffer.clear();
     return Q0;
 }
 
-state q8(string in, int &i, string &buffer)
+state q8(in &sym, string &buffer)
 {
     output << delimiters[buffer] << endl;
     buffer.clear();
@@ -200,65 +185,65 @@ state q8(string in, int &i, string &buffer)
 }
 
 bool trapped = false;
-state tp(string in, int &i, string &buffer)
+state tp(in &sym, string &buffer)
 {
-    buffer += in[i];
-    i++;
+    buffer += *(sym++);
 
     if(!trapped) 
     {
-        output << "----- TRAP -----" << endl;
+        output << "ERROR: TRAP REACHED" << endl;
         trapped = true;
     }
 
     return TRAP; // estado de erro
 }
 
-typedef state(*next_state)(string, int &, string &);
+typedef state(*next_state)(in &, string &);
 
 int main(int argc, char *argv[])
 {
     if(argc < 3)
     {
-        cout << "Uso: <arquivo_entrada> <arquivo_saida>\n";
+        cout << "Use: <input_file> <output_file>\n";
         exit(0);
     }
 
     input.open(argv[1]);
-    output.open(argv[2]);
+    output.open(strcat(argv[2], ".txt"));
    
     if(!input.good())
     {
-        cout << "Arquivo de entrada invalido.\n";
+        cout << "Error opening input file.\n";
+        remove(strcat(argv[2], ".txt"));
         exit(0);
     }
 
     if(!output.good())
     {
-        cout << "Arquivo de saida com problema.\n";
+        cout << "Error handling output file.\n";
         exit(0);
     }
    
-    stringstream code;
-    code << input.rdbuf();
+    stringstream codestream;
+    codestream << input.rdbuf();
+    string code(codestream.str());
+    in sym = code.begin();
+
+    state cur_state = Q0, end_state = TRAP;
+    string buffer;
 
     next_state delta[] = {&q0, &q1, &q2, &q3, &q4, &q5, &q6, &q7, &q8, &tp}; 
-
-    int index = 0;
-    state cur_state = Q0;
-    state end_state = TRAP;
-    string buffer;
 
     bool ended = false;
     while(!ended)
     {
-        if(index == code.str().length())
+        if(sym == code.end())
         {
             ended = true;
             end_state = cur_state;
         }
 
-        cur_state = (*delta[cur_state])(code.str(), index, buffer);
+        cur_state = (*delta[cur_state])(sym, buffer);
     }
 
     if(end_state != Q0) cout << "INPUT REJECTED\n";
